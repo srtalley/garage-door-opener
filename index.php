f20fa5a9ff684de0a1071afa84fe8a8d606b3ac3
Steve Talley <steve@dustysun.com>
2020-12-03 19:20:25 -0600

    v2020.12.03

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
 
 		<script type="text/javascript" src="viewer/jquery.md5.js"></script>
 		<script type="text/javascript" src="viewer/jquery.cookie.js"></script>
-		<script type="text/javascript" src="viewer/clear_view_cameras.js?ver=2020.11.29"></script>
+		<script type="text/javascript" src="viewer/clear_view_cameras.js?ver=2020.12.03"></script>
 
 	</head>
 	<body>
@@ -86,7 +86,7 @@
 				<div id="door2-status" class="status-line"></div>
 			</div>
 		</div>
-		<div id="cameraViewportWrapper" data-blue_iris_server="http://sol.home" data-first_cam="Garage" data-scroll_to_top="false" data-resize_viewport="false" data-refresh_rate="500" data-quality="20">
+		<div id="cameraViewportWrapper" data-blue_iris_server="http://sol.home" data-first_cam="Garage" data-scroll_to_top="false" data-resize_viewport="false" data-refresh_rate="300" data-quality="20" data-width="600">
 			<div id="cameraViewport">
 				<canvas id="cameraImg"></canvas>
 			</div>
